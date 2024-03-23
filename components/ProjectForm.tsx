639d0f99853efa0c6ba3c4349ca46582caf69b4b
Swapnil-Narendra-Patil <patil.swap@northeastern.edu>
2024-03-23 13:37:18 -0400

    New

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -113,7 +113,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
             <FormField
                 title="Title"
                 state={form.title}
-                placeholder="Flexibble"
+                placeholder="Skippy"
                 setState={(value) => handleStateChange('title', value)}
             />
 
@@ -129,7 +129,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
                 type="url"
                 title="Website URL"
                 state={form.liveSiteUrl}
-                placeholder="https://jsmastery.pro"
+                placeholder=" "
                 setState={(value) => handleStateChange('liveSiteUrl', value)}
             />
 
@@ -137,7 +137,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
                 type="url"
                 title="GitHub URL"
                 state={form.githubUrl}
-                placeholder="https://github.com/adrianhajdin"
+                placeholder=" "
                 setState={(value) => handleStateChange('githubUrl', value)}
             />
 
